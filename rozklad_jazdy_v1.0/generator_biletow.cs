8668dfc1388824d55d91ce57ef3c82ddad909033
Mateusz634 <81951019+Mateusz634@users.noreply.github.com>
2024-10-17 09:17:28 +0200

    Update generator_biletow.cs

--- rozklad_jazdy_v1.0/generator_biletow.cs
+++ rozklad_jazdy_v1.0/generator_biletow.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Drawing;
 using System.Drawing.Imaging;
-using static System.Net.Mime.MediaTypeNames;
+using QRCoder;
 
 class Example_SetJPEGQuality
 {
@@ -12,67 +12,66 @@ class Example_SetJPEGQuality
         EncoderParameter myEncoderParameter;
         EncoderParameters myEncoderParameters;
         Bitmap image1;
-   
-     
 
         image1 = new Bitmap("bilet.jpg", true);
-
         myImageCodecInfo = GetEncoderInfo("image/jpeg");
-
         myEncoder = Encoder.Quality;
-
         myEncoderParameters = new EncoderParameters(1);
-
         myEncoderParameter = new EncoderParameter(myEncoder, 25L);
         myEncoderParameters.Param[0] = myEncoderParameter;
-        image1.Save("bilet-1.jpg", myImageCodecInfo, myEncoderParameters);
 
+       
         Graphics graphics = Graphics.FromImage(image1);
-
-
         Brush brush = new SolidBrush(Color.Black);
-
-
         Font arial = new Font("Arial", 30, FontStyle.Regular);
         Font arialBoldItalic = new Font("Arial", 50, FontStyle.Bold | FontStyle.Italic);
 
-
-        string imie = "Ada,";
-        string nazwisko = "Kowalski";
+       
+        string imie = "Mateusz";
+        string nazwisko = "Janiczek";
         string cenna = "10 zl";
         string From = "Krakow";
         string to = "Warszawa";
-   
-        Rectangle rectangleImie = new Rectangle(200, 1360, 450, 100);
-        Rectangle rectangleNazwisko = new Rectangle(270, 1470, 450, 100);
-        Rectangle rectangleCena = new Rectangle(670, 1410, 450, 100);
 
-        Rectangle rectangleFrom = new Rectangle(85, 550, 450, 100);
-        Rectangle rectangleTo = new Rectangle(530, 550, 450, 100);
+        graphics.DrawString(imie, arial, brush, new Rectangle(200, 1360, 450, 100));
+        graphics.DrawString(nazwisko, arial, brush, new Rectangle(270, 1470, 450, 100));
+        graphics.DrawString(cenna, arial, brush, new Rectangle(670, 1410, 450, 100));
+        graphics.DrawString(From, arialBoldItalic, brush, new Rectangle(85, 550, 450, 100));
+        graphics.DrawString(to, arialBoldItalic, brush, new Rectangle(530, 550, 450, 100));
 
-        graphics.DrawString(imie, arial, brush, rectangleImie);
-        graphics.DrawString(nazwisko, arial, brush, rectangleNazwisko);
-        graphics.DrawString(cenna, arial, brush, rectangleCena);
-        graphics.DrawString(From, arialBoldItalic, brush, rectangleFrom);
-        graphics.DrawString(to, arialBoldItalic, brush, rectangleTo);
+        string qrText = $"{imie} {nazwisko}, {cenna}, From: {From}, To: {to}";
+        using (QRCodeGenerator qrGenerator = new QRCodeGenerator())
+        {
+            using (QRCodeData qrCodeData = qrGenerator.CreateQrCode(qrText, QRCodeGenerator.ECCLevel.L))
+            {
+                using (QRCode qrCode = new QRCode(qrCodeData))
+                {
+                    Bitmap qrCodeImage = qrCode.GetGraphic(20);
+                 
+                    graphics.DrawImage(qrCodeImage, new Rectangle(300, 1650, 350, 350)); 
+                }
+            }
+        }
 
-
+        
         image1.Save("bilet-1.jpg", myImageCodecInfo, myEncoderParameters);
 
+  
+        graphics.Dispose();
+        image1.Dispose();
     }
-    private static ImageCodecInfo GetEncoderInfo(String mimeType)
+
+    private static ImageCodecInfo GetEncoderInfo(string mimeType)
     {
-        int j;
-        ImageCodecInfo[] encoders;
-        encoders = ImageCodecInfo.GetImageEncoders();
-        for (j = 0; j < encoders.Length; ++j)
+        ImageCodecInfo[] encoders = ImageCodecInfo.GetImageEncoders();
+        for (int j = 0; j < encoders.Length; ++j)
         {
             if (encoders[j].MimeType == mimeType)
                 return encoders[j];
         }
         return null;
     }
-}   
+}
 
 
 
